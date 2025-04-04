2db1e0e97742066aafffbc219242887fa7f2daee
jgustavo <jgustavo0711@gmail.com>
2025-04-03 22:29:28 -0300

    alteracoes aplicadas em header do perfil

--- src/components/HeaderPerfil/styles.ts
+++ src/components/HeaderPerfil/styles.ts
@@ -48,7 +48,7 @@ export const TitleLink = styled(Link)`
   text-decoration: none;
   font-weight: 900;
   padding-right: 240px;
-  margin-right: 22px;
+  margin-right: 28px;
 `
 
 export const Links = styled.ul`
